2a34b7340c65e8d3406613745bf10c547db9f1ba
Chris McClure <chrismcclure2010@gmail.com>
2018-11-13 00:12:03 -0900

    initial commit of graphics project

---
+++ Assets/Scripts/BoundingBox.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoundingBox : MonoBehaviour {
+    public GameObject shape;
+    // Use this for initialization
+    void Start()
+    {
+        Vector3 boxsize = shape.GetComponent<Renderer>().bounds.size;
+        drawBox(boxsize);
+    }
+
+    void drawBox(Vector3 bsize)
+    {
+        Debug.Log(bsize.x);
+        Debug.Log(bsize.y);
+        Debug.Log(bsize.z);
+    }
+}
---
+++ Assets/Scripts/Spawn.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawn : MonoBehaviour {
+
+    public GameObject ball;
+
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetMouseButton(0))
+        {
+            {
+                Ray ray = Camera.main.ScreenPointToRay(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 0));
+                RaycastHit hit;
+                if (Physics.Raycast(ray, out hit))
+                { 
+                    Instantiate(ball, hit.point, Quaternion.identity);
+                }
+            }
+        }
+	}
+}
